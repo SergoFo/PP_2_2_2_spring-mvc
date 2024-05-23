d06bef7241c06fc14de38da1836e62f2720adc7f
Sergo Fan <sergei.fonarev@yandex.ru>
2024-05-23 19:18:18 +0300

    Update pom.xml Spring.version 6.1.7 Tomcat 10.1.24 (jakarta)

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.ArrayList;
 import java.util.List;
 
+@RequestMapping("/hello")
 @Controller
 public class HelloController {
 
